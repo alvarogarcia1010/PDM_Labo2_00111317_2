68f09526c39258cdc3d7baca465309148054c222
alvarogarcia1010 <00111317@uca.edu.sv>
2019-03-17 13:30:54 -0600

    Terminado

--- app/src/main/java/com/agarcia/galery/MainActivity.java
+++ app/src/main/java/com/agarcia/galery/MainActivity.java
@@ -4,27 +4,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
-    private LinearLayout mLinearLayout;
+    private ImageView img_1, img_2, img_3, img_4, img_5, img_6, img_7, img_8, img_9;
 
-    private Button btn1,btn2,btn3;
+    private int [] sources;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Find by id m Linear Layout
 
-        //Find by id botones
+        sources = new int[]{R.drawable.img_1, R.drawable.img_2, R.drawable.img_3, R.drawable.img_4,
+                R.drawable.img_5, R.drawable.img_6,R.drawable.img_7, R.drawable.img_8,R.drawable.img_9, R.drawable.img_10,
+                R.drawable.img_11, R.drawable.img_12,R.drawable.img_13, R.drawable.img_14,R.drawable.img_15, R.drawable.img_16,R.drawable.img_17, R.drawable.img_18};
 
-        btn1.setOnClickListener(this);
-        btn2.setOnClickListener(this);
-        btn3.setOnClickListener(this);
+        img_1 = findViewById(R.id.img_1);
+        img_2 = findViewById(R.id.img_2);
+        img_3 = findViewById(R.id.img_3);
+        img_4 = findViewById(R.id.img_4);
+        img_5 = findViewById(R.id.img_5);
+        img_6 = findViewById(R.id.img_6);
+        img_7 = findViewById(R.id.img_7);
+        img_8 = findViewById(R.id.img_8);
+        img_9 = findViewById(R.id.img_9);
+
+        img_1.setOnClickListener(this);
+        img_2.setOnClickListener(this);
+        img_3.setOnClickListener(this);
+        img_4.setOnClickListener(this);
+        img_5.setOnClickListener(this);
+        img_6.setOnClickListener(this);
+        img_7.setOnClickListener(this);
+        img_8.setOnClickListener(this);
+        img_9.setOnClickListener(this);
     }
 
     @Override
@@ -32,10 +51,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         int viewId = v.getId();
         Log.i("id", viewId+"" );
-        switch(viewId)
-        {
-            //case R.id.btn1:
-              //  break;
-        }
+
+        ImageView aux = findViewById(viewId);
+        aux.setImageResource(sources[new Random().nextInt(18)]);
     }
 }
